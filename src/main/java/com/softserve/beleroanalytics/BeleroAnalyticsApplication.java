58c32c8cef6961ddd91a49563a1536c48dbdeede
Yurii Kotsiuba <jurok3x@gmail.com>
2022-01-26 16:17:53 +0200

    first commit

---
+++ src/main/java/com/softserve/beleroanalytics/BeleroAnalyticsApplication.java
@@ -0,0 +1,13 @@
+package com.softserve.beleroanalytics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeleroAnalyticsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeleroAnalyticsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/softserve/beleroanalytics/BeleroAnalyticsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.softserve.beleroanalytics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeleroAnalyticsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
